08b2c368e3080e4a42f11083261d31f9ea8972be
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-07-12 19:29:10 +0530

    bar chart problem practice done and Sum of two Arrays

---
+++ Arrays/src/Practice_PepCoding/O3_Practice.java
@@ -0,0 +1,52 @@
+package Practice_PepCoding;
+import java.util.*;
+
+public class O3_Practice {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        // scanning done
+        for(int i = 0; i<arr.length;i++)
+        {
+            arr[i] = in.nextInt();
+
+        }
+
+        Pattern(arr);
+
+
+    }
+    public static void Pattern(int[] arr)
+    {
+        int max = arr[0];
+
+        for(int i = 0 ; i<arr.length;i++)
+        {
+            if(arr[i]>max)
+            {
+                max = arr[i];
+            }
+        }
+        // max element found
+
+        // now the part of printing
+
+        for(int floors = max ; floors>=1;floors--)
+        {
+            for(int buildings = 0 ; buildings<arr.length;buildings++)
+            {
+                if(arr[buildings]>=floors)
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                    System.out.print(" \t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
