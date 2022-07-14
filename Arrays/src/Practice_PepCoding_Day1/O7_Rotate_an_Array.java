a2c38cb24fd02b0d5924e108d04336789c6c4c2d
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-07-14 11:16:16 +0530

    debug needed

---
+++ Arrays/src/Practice_PepCoding_Day1/O7_Rotate_an_Array.java
@@ -0,0 +1,65 @@
+package Practice_PepCoding_Day1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//link -  https://www.pepcoding.com/resources/online-java-foundation/function-and-arrays/rotate-an-array-official/ojquestion
+public class O7_Rotate_an_Array {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the size of the Array:");
+        while (!in.hasNext("[0-9]+"))
+        {
+            System.out.println("Enter a valid input Enter a number:");
+            in.next();
+        }
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Please enter the elements in the array:");
+        for (int i = 0; i <arr.length ; i++)
+        {
+            arr[i] = in.nextInt();
+
+        }
+        System.out.println("OG Array = "+ Arrays.toString(arr));
+        System.out.println("Please enter the K");
+        int k=in.nextInt();
+
+        Rotate(arr,k);
+
+        System.out.printf("Rotated New Array with K=%d is "+Arrays.toString(arr),k);
+
+
+    }
+    public static void Rotate(int[] arr,int k)
+    {
+        k= k % arr.length;
+        if(k<0)
+        {
+            k=k+arr.length;
+        }
+
+        // 1st part p1
+        Reverse(arr,0,arr.length-k-1);
+        // 2nd part p2
+        Reverse(arr,arr.length-k,arr.length-1);
+
+        // 3rd part Pwhole
+        Reverse(arr,0,arr.length-1);
+
+    }
+    public static void Reverse(int[] arr , int a,int b)
+    {
+        int start = a;
+        int end = b;
+        while (start<end)
+        {
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+            start++;
+            end--;
+        }
+
+    }
+}
