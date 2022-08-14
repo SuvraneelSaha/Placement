f0ffa96e7da6fa65d84e6d0d40cbf65f898c182b
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-08-15 00:18:26 +0530

    Question 4 till done

--- Matrix_Intermediate_BinaryWorld/src/O2.java
+++ Matrix_Intermediate_BinaryWorld/src/O2.java
@@ -1,15 +1,12 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class O1 {
+public class O2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter the size of the row :");
         int n = in.nextInt();
-        System.out.println("Please enter the size of the column :");
-        int m = in.nextInt();
-
-        int[][] arr = new int[n][m];
+        int[][] arr = new int[n][n];
         System.out.println("Please enter the elements in the Array:");
         for (int row = 0; row < n; row++)
         {
@@ -19,7 +16,6 @@ public class O1 {
 
             }
         }
-
         System.out.println("Original Matrix :");
         for (int[] row : arr)
         {
@@ -27,17 +23,19 @@ public class O1 {
         }
 
         // creating a new array of the same size
-        int[][] brr =new int[n][m];
+        int[][] brr =new int[n][n];
 
 
-
-        for (int row = 0; row < arr.length; row++)
+        // Actual Operation
+        int k =0 ;
+        for (int row = n-1; row >= 0; row--)
         {
-            for (int col = m-1,k=0; col >=0 ; col--)
+            for (int col = 0; col < n ; col++)
             {
-                brr[k][row] = arr[row][col];
-                k++;
+                brr[col][row] = arr[k][col];
+
             }
+            k++;
         }
 
         System.out.println("New Array:");
--- Matrix_Intermediate_BinaryWorld/src/O3.java
+++ Matrix_Intermediate_BinaryWorld/src/O3.java
@@ -1,15 +1,12 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class O1 {
+public class O3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter the size of the row :");
         int n = in.nextInt();
-        System.out.println("Please enter the size of the column :");
-        int m = in.nextInt();
-
-        int[][] arr = new int[n][m];
+        int[][] arr = new int[n][n];
         System.out.println("Please enter the elements in the Array:");
         for (int row = 0; row < n; row++)
         {
@@ -19,7 +16,6 @@ public class O1 {
 
             }
         }
-
         System.out.println("Original Matrix :");
         for (int[] row : arr)
         {
@@ -27,25 +23,7 @@ public class O1 {
         }
 
         // creating a new array of the same size
-        int[][] brr =new int[n][m];
-
-
-
-        for (int row = 0; row < arr.length; row++)
-        {
-            for (int col = m-1,k=0; col >=0 ; col--)
-            {
-                brr[k][row] = arr[row][col];
-                k++;
-            }
-        }
-
-        System.out.println("New Array:");
-        for (int[] row : brr)
-        {
-            System.out.println(Arrays.toString(row));
-        }
-
-
+        int[][] brr =new int[n][n];
+        `
     }
 }
