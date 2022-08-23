dc92e10e882b9b94a3d6ae3db57fafb8ea716358
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-08-24 00:44:57 +0530

    updated O11

--- Methods_BinaryWorld/src/O11.java
+++ Methods_BinaryWorld/src/O11.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 public class O11 {
     public static void main(String[] args) {
         sumofDigits();
-        System.err.println();
-        System.err.println();
-        sumofDigits();
     }
     static void sumofDigits()
     {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter the number ");
         int n = in.nextInt();
+        int temp = n ;
         int sum = 0 ;
         while (n!=0)
         {
@@ -19,7 +17,8 @@ public class O11 {
             sum = sum+rem;
             n=n/10;
         }
-        System.out.printf("the sum of the digits of %d is %d",n,sum);
+        System.out.printf("the sum of the digits of %d is %d",temp,sum);
+        System.err.println();
 
 
     }
